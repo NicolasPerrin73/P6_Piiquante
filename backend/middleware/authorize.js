0904658dfd9fe4273c52a07c96eb46e581c85600
NicolasPerrin73 <nicobolas7338@gmail.com>
2022-07-20 17:41:45 +0200

    add multer and sauce schema for mongoose

---
+++ backend/middleware/authorize.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "2D5815741D64B4DC81C53C0210DEBC3B92BF72FC77CCE83F61E85FBAA5200285");
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
