d4c100442236ed7bd6fb4b07a95ea039da72b7e6
NicolasPerrin73 <nicobolas7338@gmail.com>
2022-07-20 13:20:08 +0200

    backend start

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://NicolasPerrin:pzBQKEFUN7qNunKs@cluster0.egaidzy.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
